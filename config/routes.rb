3951c84130ce1937820b6113958982e1508d90a4
Yurina.Sato <yuplico5221@gmail.com>
2020-11-03 23:17:17 +0900

    tripsコントローラーのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [:show]
+  resources :trips, only: [:show, :new, :create, :edit, :update, :destroy]
+
   root to: "users#show"
 end
