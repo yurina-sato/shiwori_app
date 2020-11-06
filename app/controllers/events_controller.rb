c31b9620999ae8aca2109e6c3ff4f7a688d6893f
Yurina.Sato <yuplico5221@gmail.com>
2020-11-06 16:36:00 +0900

    eventsコントローラーを作成

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,65 @@
+class EventsController < ApplicationController
+  before_action :set_trip
+  before_action :set_schedule
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_root
+
+
+  def show
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to trip_schedule_path(@trip.id, @schedule.id), notice: 'イベントを作成しました。'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @event.update(event_params)
+      redirect_to trip_schedule_event_path(@trip.id, @schedule.id, @event.id), notice: 'イベントを編集しました。'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @event.destroy
+      redirect_to trip_schedule_path(@trip.id, @schedule.id), notice: 'イベントを削除しました。'
+    else
+      flash.now[:alart] = '削除できないイベントです。'
+      redirect_to trip_schedule_path(@trip.id, @schedule.id)
+    end
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:name, :text, :day, :start_time, :finish_time, :place, :url, :price).merge(schedule_id: params[:schedule_id])
+  end
+
+  def set_trip
+    @trip = Trip.find(params[:trip_id])
+  end
+
+  def set_schedule
+    @schedule = Schedule.find(params[:schedule_id])
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def move_to_root
+    redirect_to root_path unless user_signed_in? && @trip.user_ids.include?(current_user.id)
+  end
+
+end
