d3bef5abf1a24b3e05f1a2c68065d9773503d7e9
Yurina.Sato <yuplico5221@gmail.com>
2020-11-06 16:34:20 +0900

    Eventモデルを記述

---
+++ app/models/event.rb
@@ -0,0 +1,24 @@
+class Event < ApplicationRecord
+  belongs_to :schedule
+
+  # has_many_attached :images, dependent: :destroy
+
+  with_options presence: true do
+    validates :name, :day, :start_time, :finish_time
+  end
+
+
+  validate :date_before_start
+  validate :time_check
+
+  def date_before_start # 開始日は今日以降の日付
+    return if day.blank? # 空かどうかを判定
+    errors.add(:day, "は今日以降の日付を選択してください") if self.day < Date.today
+  end
+
+  def time_check # 時刻の前後をチェック
+    return if finish_time.blank? || start_time.blank? # 空かどうかを判定
+    errors.add(:finish_time, "の時刻を正しく記入して下さい") unless self.start_time < self.finish_time
+  end 
+
+end
