b81a5d9ccadd0e1d1806f99b078401f043a9d1dd
Yurina.Sato <yuplico5221@gmail.com>
2020-11-06 23:29:07 +0900

    Eventモデルを修正(image追加、バリデーション修正)

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,20 +1,19 @@
 class Event < ApplicationRecord
   belongs_to :schedule
-
+  has_one_attached :image
   # has_many_attached :images, dependent: :destroy
 
   with_options presence: true do
     validates :name, :day, :start_time, :finish_time
   end
 
-
-  validate :date_before_start
   validate :time_check
+   # validate :date_before_start # 旅行後のアルバムとして、日付のバリデーションは無しとする
 
-  def date_before_start # 開始日は今日以降の日付
-    return if day.blank? # 空かどうかを判定
-    errors.add(:day, "は今日以降の日付を選択してください") if self.day < Date.today
-  end
+  # def date_before_start # 開始日は今日以降の日付
+  #   return if day.blank? # 空かどうかを判定
+  #   errors.add(:day, "は今日以降の日付を選択してください") if self.day < Date.today
+  # end
 
   def time_check # 時刻の前後をチェック
     return if finish_time.blank? || start_time.blank? # 空かどうかを判定
