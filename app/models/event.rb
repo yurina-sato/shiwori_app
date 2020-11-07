cf9158a5adfb9d3eb359862110012df04020ac95
Yurina.Sato <yuplico5221@gmail.com>
2020-11-07 21:15:37 +0900

    Eventモデルの日付バリデーションを仮で記述

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,12 +7,12 @@ class Event < ApplicationRecord
   end
 
   validate :time_check
-   # validate :date_before_start # 旅行後のアルバムとして、日付のバリデーションは無しとする
+  validate :date_before_start # 旅行後のアルバムとして、日付のバリデーションは無しとする?
 
-  # def date_before_start # 開始日は今日以降の日付
-  #   return if day.blank? # 空かどうかを判定
-  #   errors.add(:day, "は今日以降の日付を選択してください") if self.day < Date.today
-  # end
+  def date_before_start # 開始日は今日以降の日付
+    return if day.blank? # 空かどうかを判定
+    errors.add(:day, "は今日以降の日付を選択してください") if self.day < Date.today
+  end
 
   def time_check # 時刻の前後をチェック
     return if finish_time.blank? || start_time.blank? # 空かどうかを判定
