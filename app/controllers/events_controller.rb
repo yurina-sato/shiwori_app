2cfd9d1031296c12e24ebad902ca3f7fd3f25567
Yurina.Sato <yuplico5221@gmail.com>
2020-11-08 17:02:10 +0900

    listsコントローラーを修正(lists#showにitem一覧を表示)

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,7 +13,6 @@ class EventsController < ApplicationController
   end
 
   def create
-    # binding.pry
     @event = Event.new(event_params)
     if @event.save
       redirect_to trip_schedule_path(@trip.id, @schedule.id), notice: 'イベントを作成しました。'
